05946d88dbfe8f42c75867cd292755b93a733dc1
fDero <francescodero@outlook.it>
2026-03-20 18:31:08 +0100

    Use bson annotations for UserModel

--- src/model/UserModel.kt
+++ src/model/UserModel.kt
@@ -1,15 +1,25 @@
 package model
 
 import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.codecs.pojo.annotations.BsonCreator
+import org.bson.codecs.pojo.annotations.BsonProperty
 import org.bson.types.ObjectId
 
-data class UserModel(
-    
-    @field:BsonId 
+data class UserModel @BsonCreator constructor(
+
+    @param:BsonId 
+    @param:BsonProperty("_id")
     val id: ObjectId? = null,
-    
+
+    @param:BsonProperty("username")
     val username: String,
-    val email:    String,
-    val tenant:   String,
-    val status:   UserStatus,
+
+    @param:BsonProperty("email")
+    val email: String,
+
+    @param:BsonProperty("tenant")
+    val tenant: String,
+
+    @param:BsonProperty("status")
+    val status: UserStatus,
 )
